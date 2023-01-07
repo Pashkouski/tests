349458be58cae50d109f329b2d760db21672aa36
pashkouski <satiym@gmail.com>
2023-01-07 18:05:34 +0300

    2.1

---
+++ src/02/02_02.ts
@@ -0,0 +1,31 @@
+export type StreetType = {
+    title: string
+}
+
+
+export type AddressType = {
+    number?: number
+    street: StreetType
+}
+
+export type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: AddressType
+}
+
+export type CityType = {
+    title: string
+    houses: Array<HouseType>
+    governmentBuildings: Array<GovernmentBuildingType>
+    citizensNumber: number
+}
+
+export type GovernmentBuildingType = {
+    type: "HOSPITAL" | "FIRE-STATION"
+    budget: number
+    staffCount: number
+    address: AddressType
+}
+
+
